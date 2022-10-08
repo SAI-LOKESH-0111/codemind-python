2a659c064141b6ba9c4db2fa02c000b02e752326
SAI-LOKESH-0111 <109729968+SAI-LOKESH-0111@users.noreply.github.com>
2022-10-08 14:49:06 +0530

    n,m = map(int,input().split()) while n != 0 and m != 0:     if n>m:         n %= m     else:         m %= n if m == 0:     print(n) else:     print(m)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,10 +1,10 @@
-n,m=map(int,input().split())
+n,m = map(int,input().split())
 while n != 0 and m != 0:
     if n>m:
         n %= m
     else:
         m %= n
-if m==0:
+if m == 0:
     print(n)
 else:
     print(m)
